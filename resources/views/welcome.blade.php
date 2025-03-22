3fd4be3faa27705b7776d984685daa4e1eff4cbb
nichiyoo <61191577+nichiyoo@users.noreply.github.com>
2025-03-23 05:59:11 +0700

    fix: change z order for homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,13 @@
       </div>
     </div>
 
+    <div class="absolute bottom-0 w-full min-w-ornament">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path class="fill-secondary" fill-opacity="1"
+          d="M0,96L60,96C120,96,240,96,360,117.3C480,139,600,181,720,192C840,203,960,181,1080,165.3C1200,149,1320,139,1380,133.3L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z">
+        </path>
+      </svg>
+    </div>
 
     <main class="container relative grid items-center min-h-screen gap-10 max-w-7xl">
       <div class="grid items-center gap-10 lg:grid-cols-3">
@@ -40,14 +47,6 @@
         </div>
       </div>
     </main>
-
-    <div class="absolute bottom-0 w-full min-w-ornament">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path class="fill-secondary" fill-opacity="1"
-          d="M0,96L60,96C120,96,240,96,360,117.3C480,139,600,181,720,192C840,203,960,181,1080,165.3C1200,149,1320,139,1380,133.3L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z">
-        </path>
-      </svg>
-    </div>
 
     <div class="absolute bottom-0 w-full py-10 text-center">
       <span class="font-bold text-zinc-900">
